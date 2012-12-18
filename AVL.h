e91403def9f7ff87631cfb5674292f8b1b574eb5
jonnyhsy <jonnyhsy@foxmail.com>
2012-12-18 02:57:45 -0500

    update .h

--- AVL.h
+++ AVL.h
@@ -13,56 +13,17 @@ typedef struct AVL{
 
 AVL* New_Node(KEY_TYPE key, AVL* lchild, AVL* rchild, int height = -1);
 inline int getHeight(AVL* node);
-/* RR(Y rotates to the right):
-
-		k2                   k1
-	   /  \           		/  \
-	  k1   Z     ==>       X   k2
-	 / \                      /  \
-    X   Y                    Y    Z
-*/
 /*
  Return which the root pointer(at a higher level) should point to
  */
 AVL* RR_Rotate(AVL* k2);
-
-/* LL(Y rotates to the left):
-
-      k2                       k1
-	 /  \                     /  \
-	X    k1         ==>      k2   Z
-		/  \                /  \
-	   Y    Z              X    Y
- */
 AVL* LL_Rotate(AVL* k2);
-
-/* LR(B rotates to the left, then C rotates to the right):
-
-      k3                         k3                       k2
-	 /  \                       /  \                     /  \
-	k1   D                     k2   D                   k1   k3
-   /  \         ==>           /  \        ==>          / \   / \
-  A    k2                    k1   C                   A  B  C   D
-      /  \                  /  \
-	 B    C                A    B
-
-*/
 /*
  Return which the root pointer should point to
  */
 AVL* LR_Rotate(AVL* k3);
 
-/* RL(D rotates to the right, then C rotates to the left):
-
-       k3                         k3                          k2
-	  /  \                       /  \                        /  \
-	 A    k1                    A    k2                     k3   k1 
-	     /  \       ==>             /  \         ==>       /  \  / \
-		k2   B                     C    k1                A   C D   B
-	   /  \                            /  \
-	  C    D                          D    B 
-
- */
+/* RL(D rotates to the right, then C rotates to the left):*/
 AVL* RL_Rotate(AVL* k3);
 
 
