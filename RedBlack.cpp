0e94a35a244e7bf5cfb3282f0226d9321fb9174a
Andrea Tupini <tupini07@gmail.com>
2015-07-03 17:59:43 -0600

    RedBlack tree insert

--- RedBlack.cpp
+++ RedBlack.cpp
@@ -1,2 +1,218 @@
 #include "RedBlack.h"
+#include <stdio.h>
+#include <assert.h>
 
+
+node* RedBlack::Grandparent(node* n){
+    if(n->parent != NULL)
+        return n->parent->parent;
+    else 
+        return NULL;
+}
+
+node* RedBlack::Sibling(node* n){
+    node* parent = n->parent;
+    if(parent == NULL) return NULL;
+    
+    if(parent->left == n)
+        return parent->right;
+    
+    else if(parent->right == n)
+        return parent->left;
+}
+
+node* RedBlack::Uncle(node* n){
+    node* parent = n->parent;
+    node* grandparent = Grandparent(n);
+    if(parent == NULL || grandparent == NULL) return NULL;
+    
+    if(grandparent->left == parent)
+        return grandparent->right;
+    
+    else if(grandparent->right == parent)
+        return grandparent->left;
+}
+
+void RedBlack::Right_Rotate(node* k2){
+    node* k1 = k2->left;
+    k2->left = k1->right;
+    k1->right = k2;
+}
+
+void RedBlack::Left_Rotate(node* k2){
+    node* k1 = k2->right;
+    k2->right = k1->left;
+    k1->left = k2;
+}
+
+/* If it's NULL then it means it's a leaf, so it's color is BLACK */
+color RedBlack::node_color(node* n){
+    return n == NULL ? BLACK : n->color;
+}
+
+//Verify properties [rules]
+void RedBlack::verify_properties(){
+    verify_property_1(this->root);
+    verify_property_2(this->root);
+    verify_property_4(this->root);
+    verify_property_5(this->root);
+}
+
+void RedBlack::verify_property_1(node* root){
+    assert (node_color(root) == RED || node_color(root) == BLACK);
+    if (root == NULL)
+        return;
+    verify_property_1(root->left);
+    verify_property_1(root->right);
+}
+
+void RedBlack::verify_property_2(node* root){
+    assert (node_color(root) == BLACK);
+}
+
+void RedBlack::verify_property_4(node* root){
+    if (node_color(root) == RED)
+    {
+        assert (node_color(root->left) == BLACK);
+        assert (node_color(root->right) == BLACK);
+        assert (node_color(root->parent) == BLACK);
+    }
+    if (root == NULL)
+        return;
+    verify_property_4(root->left);
+    verify_property_4(root->right);
+}
+
+//The number of blacks from the root to a leaf must be the same for every path
+void RedBlack::verify_property_5(node* root){
+    int black_count_path = -1;
+    verify_property_5_rec(root, 0, &black_count_path);
+}
+void RedBlack::verify_property_5_rec(node* n, int black_count, int* path_black_count){
+    if (node_color(n) == BLACK)
+    {
+        black_count++;
+    }
+    if (n == NULL)
+    {
+        if (*path_black_count == -1)
+        {
+            *path_black_count = black_count;
+        }
+        else
+        {
+            assert (black_count == *path_black_count);
+        }
+        return;
+    }
+    verify_property_5_rec(n->left,  black_count, path_black_count);
+    verify_property_5_rec(n->right, black_count, path_black_count);
+}
+
+
+/* Insertion is the same as in a normal binary tree */
+void RedBlack::Insert(int _key){
+    rbtree_node* node_insert = new rbtree_node();
+    node_insert->color = RED;
+    node_insert->key = _key;
+    if (this->root == NULL)
+    {
+        this->root = node_insert;
+    }
+    else
+    {
+        node* n = this->root;
+        //iterative insert
+        while (1)
+        {
+            if (n->key == node_insert->key)
+            {
+                return;
+            }
+            else if (node_insert->key < n->key)
+            {
+                if (n->left == NULL)
+                {
+                    n->left = node_insert;
+                    break;
+                }
+                else
+                {
+                    n = n->left;
+                }
+            }
+            else
+            {
+                assert (node_insert->key > n->key);
+                if (n->right == NULL)
+                {
+                    n->right = node_insert;
+                    break;
+                }
+                else
+                {
+                    n = n->right;
+                }
+            }
+        }
+        node_insert->parent = n;
+    }
+    insert_case1(node_insert);
+    verify_properties();
+}
+
+void RedBlack::insert_case1(node* n){
+    if (n->parent == NULL)
+        n->color = BLACK;
+    else
+        insert_case2(n);
+}
+void RedBlack::insert_case2(node* n){
+    if (node_color(n->parent) == BLACK)
+        return;
+    else
+        insert_case3(n);
+}
+void RedBlack::insert_case3(node* n){
+    if (node_color(Uncle(n)) == RED)
+    {
+        n->parent->color = BLACK;
+        Uncle(n)->color = BLACK;
+        Grandparent(n)->color = RED;
+        insert_case1(Grandparent(n));
+    }
+    else
+    {
+        insert_case4(n);
+    }
+}
+void RedBlack::insert_case4(node* n){
+    if (n == n->parent->right && n->parent == Grandparent(n)->left)
+    {
+        Left_Rotate(n->parent);
+        n = n->left;
+    }
+    else if (n == n->parent->left && n->parent == Grandparent(n)->right)
+    {
+        Right_Rotate(n->parent);
+        n = n->right;
+    }
+    insert_case5(n);
+}
+void RedBlack::insert_case5(node* n){
+    n->parent->color = BLACK;
+    Grandparent(n)->color = RED;
+    if (n == n->parent->left && n->parent == Grandparent(n)->left)
+    {
+        Right_Rotate(Grandparent(n));
+    }
+    else
+    {
+        assert (n == n->parent->right && n->parent == Grandparent(n)->right);
+        Left_Rotate(Grandparent(n));
+    }
+}
+
+void RedBlack::Delete(int _key){
+    
+}
--- RedBlack.h
+++ RedBlack.h
@@ -1,28 +1,54 @@
 #ifndef REDBLACK_H
 #define	REDBLACK_H
-enum color { RED, BLACK };
 
-typedef struct rbtree_node
-{
+/* 
+ * Rules of a Red/Black tree 
+ *      1. A node is either red or black.
+ *      2. The root is black.
+ *      3. All leaves (NIL) are black. All leaves are of the same color as the root.
+ *      4. Every red node must have two black child nodes, and therefore it must have a black parent.
+ *      5. Every path from a given node to any of its descendant NIL nodes contains the same number of black nodes.
+ * [source: https://en.wikipedia.org/wiki/Red%E2%80%93black_tree#Properties]
+ */
+enum color {
+    RED, BLACK
+};
+
+typedef struct rbtree_node {
     enum color color;
     int key;
     rbtree_node *left, *right, *parent;
-}* node;
+} node;
 
 class RedBlack {
 private:
-    rbtree_node root;
-    int compare(int a, int b);  //returns 0 if equal or a>b ? 1 : -1
-    node* grandparent(node* n);
-    node* sibling(node* n);
-    node* uncle(node* n);
-    bool verify_case1(node* node);
-    bool verify_case2(node* node);
-    bool verify_case3(node* node);
-    node new_node(color, int key, rbtree_node* left, rbtree_node* right, rbtree_node* parent);
+    rbtree_node* root;
+    node* Grandparent(node* n);
+    node* Sibling(node* n);
+    node* Uncle(node* n);
+    void Right_Rotate(node* k2);
+    void Left_Rotate(node* k2);
+    color node_color(node* n);
+
+    //verify rules are not broken
+    void verify_properties();
+    void verify_property_1(node* root);
+    void verify_property_2(node* root);
+    void verify_property_4(node* root);
+    void verify_property_5(node* root);
+    void verify_property_5_rec(node* n, int black_count, int* path_black_count);
+
+    //insert steps
+    void insert_case1(node* n);
+    void insert_case2(node* n);
+    void insert_case3(node* n);
+    void insert_case4(node* n);
+    void insert_case5(node* n);
+    
 public:
     void Insert(int _key);
     void Delete(int _key);
+    void Display();
 
 };
 
