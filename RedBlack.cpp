7c5d6ebdf58d50046397cf20f6416fb1b887d5ab
Andrea Tupini <tupini07@gmail.com>
2015-07-03 14:31:53 -0600

    more methods specification RebBlack.h

--- RedBlack.cpp
+++ RedBlack.cpp
@@ -1,18 +1,2 @@
-/* 
- * File:   RedBlack.cpp
- * Author: andrea
- * 
- * Created on July 3, 2015, 1:59 PM
- */
-
 #include "RedBlack.h"
-
-RedBlack::RedBlack() {
-}
-
-RedBlack::RedBlack(const RedBlack& orig) {
-}
-
-RedBlack::~RedBlack() {
-}
 
--- RedBlack.h
+++ RedBlack.h
@@ -13,6 +13,13 @@ class RedBlack {
 private:
     rbtree_node root;
     int compare(int a, int b);  //returns 0 if equal or a>b ? 1 : -1
+    node* grandparent(node* n);
+    node* sibling(node* n);
+    node* uncle(node* n);
+    bool verify_case1(node* node);
+    bool verify_case2(node* node);
+    bool verify_case3(node* node);
+    node new_node(color, int key, rbtree_node* left, rbtree_node* right, rbtree_node* parent);
 public:
     void Insert(int _key);
     void Delete(int _key);
