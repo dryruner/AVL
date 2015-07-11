213d88276794fb24b9426bd5a5a499006bc39739
Andrea Tupini <tupini07@gmail.com>
2015-07-10 22:44:00 -0600

    conectado todo

--- BigTree.h
+++ BigTree.h
@@ -17,60 +17,114 @@ Source [http://www.geeksforgeeks.org/b-tree-set-1-introduction-2/]
  */
 #include<iostream>
 using namespace std;
- 
-// A BTree node
-class BTreeNode
-{
-    int *keys;  // An array of keys
-    int t;      // Minimum degree (defines the range for number of keys)
-    BTreeNode **C; // An array of child pointers
-    int n;     // Current number of keys
-    bool leaf; // Is true when node is leaf. Otherwise false
+
+/* Clase que va a representar a un nodo del arbol */
+class BTreeNode {
+    
+private:
+    int * keys; // Arreglo de numeros en el nodo
+    int degree; // Define el grado del nodo (este es compartido por todos los nodos del arbol)
+    BTreeNode ** children; // Un arreglo de punters hacia los nodos hijos
+    int number_keys; //numero actual de numeros (keys)  que se encuentran en el nodo
+    bool leaf; // Es un nodo hoja o no
+    
 public:
-    BTreeNode(int _t, bool _leaf);   // Constructor
- 
+    BTreeNode(int _t, bool _leaf); // Constructor
+
+    // A function to traverse all nodes in a subtree rooted with this node
+    void traverse();
+
+    // A function to search a key in subtree rooted with this node.
+    BTreeNode *search(int k); // returns NULL if k is not present.
+
+    // A function that returns the index of the first key that is greater
+    // or equal to k
+    int findKey(int k);
+
     // A utility function to insert a new key in the subtree rooted with
     // this node. The assumption is, the node must be non-full when this
     // function is called
     void insertNonFull(int k);
- 
-    // A utility function to split the child y of this node. i is index of y in
-    // child array C[].  The Child y must be full when this function is called
+
+    // A utility function to split the child y of this node. i is index
+    // of y in child array C[].  The Child y must be full when this
+    // function is called
     void splitChild(int i, BTreeNode *y);
- 
-    // A function to traverse all nodes in a subtree rooted with this node
-    void traverse();
- 
-    // A function to search a key in subtree rooted with this node.
-    BTreeNode *search(int k);   // returns NULL if k is not present.
- 
-// Make BTree friend of this so that we can access private members of this
-// class in BTree functions
-friend class BigTree;
+
+    // A wrapper function to remove the key k in subtree rooted with
+    // this node.
+    void remove(int k);
+
+    // A function to remove the key present in idx-th position in
+    // this node which is a leaf
+    void removeFromLeaf(int idx);
+
+    // A function to remove the key present in idx-th position in
+    // this node which is a non-leaf node
+    void removeFromNonLeaf(int idx);
+
+    // A function to get the predecessor of the key- where the key
+    // is present in the idx-th position in the node
+    int getPred(int idx);
+
+    // A function to get the successor of the key- where the key
+    // is present in the idx-th position in the node
+    int getSucc(int idx);
+
+    // A function to fill up the child node present in the idx-th
+    // position in the C[] array if that child has less than t-1 keys
+    void fill(int idx);
+
+    // A function to borrow a key from the C[idx-1]-th node and place
+    // it in C[idx]th node
+    void borrowFromPrev(int idx);
+
+    // A function to borrow a key from the C[idx+1]-th node and place it
+    // in C[idx]th node
+    void borrowFromNext(int idx);
+
+    // A function to merge idx-th child of the node with (idx+1)th child of
+    // the node
+    void merge(int idx);
+
+    // Make BTree friend of this so that we can access private members of
+    // this class in BTree functions
+    friend class BigTree;
 };
 
 class BigTree {
-public:
-    void BigMenu();
-    
-        // Constructor (Initializes tree as empty)
-    BigTree(int _t)
-    {  root = NULL;  t = _t; }
- 
-    // function to traverse the tree
-    void traverse()
-    {  if (root != NULL) root->traverse(); }
- 
-    // function to search a key in this tree
-    BTreeNode* search(int k)
-    {  return (root == NULL)? NULL : root->search(k); }
-    
-     // The main function that inserts a new key in this B-Tree
-    void insert(int k);
-    
-private:
+ private:
     BTreeNode *root; // Pointer to root node
-    int t;  // Minimum degree
+    int tree_degree; // Minimum degree
+
+public:
+   static void BigMenu();
+
+    // Constructor (Initializes tree as empty)
+
+    BigTree(int _degree) {
+        root = NULL;
+        tree_degree = _degree;
+    }
+
+    void traverse() {
+        if (root != NULL) root->traverse();
+    }
+
+    // function to search a key in this tree
+
+    BTreeNode* search(int k) {
+        return (root == NULL) ? NULL : root->search(k);
+    }
+
+    // The main function that inserts a new key in this B-Tree
+    void insert(int k);
+
+    // The main function that removes a new key in thie B-Tree
+    void remove(int k);
+
+
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -6,43 +6,38 @@
 #include "BigTree.h"
 using namespace std;
 
+int main(int argc, char* argv[]) {
+    int opcion = 0;
+    do {
+        system("cls");
+        cout << "||Menú de selección de ejemplos de Árboles||" << endl;
+        cout << "1 - AVL" << endl;
+        cout << "2 - Árbol Rojo-Negro" << endl;
+        cout << "3 - Árbol B" << endl;
+        cout << "0 - Terminar" << endl;
+        cout << " Seleccione una opcion -> ";
+        cin >> opcion;
 
-int main(int argc, char* argv[])
-{
-	int opcion = 0;
-	do {
-		system("cls");
-                cout << "||Menú de selección de ejemplos de Árboles||" << endl;
-		cout << "1 - AVL" << endl;
-		cout << "2 - Árbol Rojo-Negro" << endl;
-		cout << "3 - Árbol B" << endl;
-		cout << "0 - Terminar" << endl;
-		cout << " Seleccione una opcion -> ";
-		cin >> opcion;
-
-		switch (opcion)
-		{
-		case 1:
-			AVLTree::AVLmenu();
-			break;
-		case 2:
-                    RedBlack::RBMenu();
-                    break;
-		case 3:
-                    break;
-		
-
-		
-		default:
-			if (opcion != 0) {
-				cout << "Opción no Valida" << endl;
-				system("pause");
-			}
-			break;
-		}
-	} while (opcion != 0);
-	cout << "Fin del programa" << endl;
-	system("pause");
+        switch (opcion) {
+            case 1:
+                AVLTree::AVLmenu();
+                break;
+            case 2:
+                RedBlack::RBMenu();
+                break;
+            case 3:
+                BigTree::BigMenu();
+                break;
+            default:
+                if (opcion != 0) {
+                    cout << "Opción no Valida" << endl;
+                    system("pause");
+                }
+                break;
+        }
+    } while (opcion != 0);
+    cout << "Fin del programa" << endl;
+    system("pause");
 }
 
 
